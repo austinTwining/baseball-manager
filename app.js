52f96824524c88559aa7ccbd1c47bf8a0ac88e30
Austin-James Twining <austin.twining@gmail.com>
2019-08-20 12:27:24 -0400

    added react... again..

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const port = process.env.PORT || 5000;
 
 var playersRouter = require('./api/players');
 
-//app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 //api endpoint router
 app.use('/api/players', playersRouter);
 
 //catchall handler
-/*app.get('*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});*/
+});
 
 app.listen(port, () => {
   console.log(`server started on port ${port}...`);
