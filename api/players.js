888570b96a83e7a0e104488042446ad44d4c072f
Austin-James Twining <austin.twining@gmail.com>
2019-08-20 23:11:15 -0400

    added some RESTful routes to players

--- api/players.js
+++ api/players.js
@@ -20,5 +20,38 @@ var players = [
 router.get('/', (req, res) => {
     res.json(players);
 });
+router.get('/:id', (req, res) => {
+  var currPlayer = players.filter((player) => {
+    if(player.id == req.params.id) return true;
+  });
+  if(currPlayer.length == 1){
+    res.json(currPlayer[0])
+  } else {
+    res.status(404);//Set status to 404 as movie was not found
+    res.json({message: "Not Found"});
+ }
+  res.json(players);
+});
+router.post('/', (req, res) => {
+  if(!req.body.name){
+    res.status(400);
+    res.json("bad request!");
+  }else{
+    var newId = players[players.length-1].id+1;
+    players.push({
+       id: newId,
+       name: req.body.name
+    });
+    res.json({message: "new player created", location: "/api/players/" + newId});
+  }
+});
+router.put('/:id', (req, res) => {
+  res.status(400);
+  res.json("bad request!");
+});
+router.delete('/:id', (req, res) => {
+  res.status(400);
+  res.json("bad request!");
+});
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
@@ -8,6 +9,10 @@ const port = process.env.PORT || 5000;
 var playersRouter = require('./api/players');
 
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //api endpoint router
 app.use('/api/players', playersRouter);
